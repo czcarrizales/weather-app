efec9e9cfc0926d571bc364105863e1bbb16faf6
Chazz Carrizales <czcarrizales@yahoo.com>
2023-09-25 20:34:58 -0700

    getting weatherData for all

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import {format} from 'date-fns'
+import  {geocodeByAddress, getLatLng} from 'react-places-autocomplete'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TemperatureTrends from './TemperatureTrends';
+import WindSpeedTrends from './WindSpeedTrends';
+import DefaultWeatherView from './DefaultWeatherView';
+import CompareWeather from './CompareWeather';
+import NavigationBar from './NavigationBar';
 
 function App() {
 
   const [weatherData, setWeatherData] = useState<any>(null)
-  const [temperatureChartData, setTemperatureChartData] = useState(null)
-  const [windChartData, setWindChartData] = useState(null)
-  const [minChartTemperature, setMinChartTemperature] = useState(null)
-  const [maxChartTemperature, setMaxChartTemperature] = useState(null)
-  const [minChartDT, setMinChartDT] = useState(null)
-  const [maxChartDT, setMaxChartDT] = useState(null)
-  const [temperature, setTemperature] = useState(null)
-  const [unit, setUnit] = useState<string | null>(null);
-  const [locationQuery, setLocationQuery] = useState(null)
-  const apiKey = 'e17d9f28655ac27f972639f336659737'
+  const [dailyWeatherData, setDailyWeatherData] = useState(null)
 
-  const getData = async () => {
-    try {
-      const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e17d9f28655ac27f972639f336659737')
-      setWeatherData(response.data)
-      console.log(response.data)
-    } catch (error) {
-      console.error('Error getting the weather data: ', error)
-    }
-  }
-
-  const getCurrentLocationWeatherData = async () => {
+  const getWeatherData = async () => {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
         async (position) => {
@@ -48,116 +34,66 @@ function App() {
     }
   }
 
-  const getRandomLocationWeatherData = async () => {
-    const randomLatitude = -90 + (Math.random() * (90 - -90));
-    const randomLongitude = -180 + (Math.random() * (180 - -180));
+  const getDailyWeatherData = async (lat: number, lon: number) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${randomLatitude}&lon=${randomLongitude}&appid=e17d9f28655ac27f972639f336659737`)
-      setWeatherData(response.data)
-      setTemperature(response.data.main.temp)
-      setUnit('kelvin')
-      console.log(response.data)
+      const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timeformat=unixtime&timezone=auto`)
+      console.log(response, 'daily weather')
+      const dailyWeatherDataArray = response.data.daily
+      console.log(dailyWeatherDataArray)
+      setDailyWeatherData(dailyWeatherDataArray)
     } catch (error) {
-      console.error('Error getting current weather data:', error)
+      console.log(error)
     }
   }
 
-  const handleInputChange = (event: string) => {
-    setLocationQuery(event.target.value)
+  const searchWeatherData = async (lat: number, lon: number) => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e17d9f28655ac27f972639f336659737`)
+      getDailyWeatherData(lat, lon)
+      setWeatherData(response.data)
+    } catch (error) {
+      console.error('Error searching and getting current weather data:', error)
+    }
   }
 
-  const displayKelvin = () => {
-    const kelvin = weatherData?.main.temp
-    setTemperature(kelvin)
-    setUnit('kelvin')
+  const convertToFahrenheit = (number: number) => {
+    const fahrenheit = ((number - 273.15) * (9 / 5) + 32).toFixed(0);
+    return fahrenheit
   }
 
-  const displayFahrenheit = () => {
-      const fahrenheit = ((weatherData?.main.temp - 273.15) * (9 / 5) + 32).toFixed(2);
-      setTemperature(fahrenheit);
-      setUnit('fahrenheit');
-  }
+  const [address, setAddress] = useState('');
+  const handleChange = (newAddress: string) => {
+    setAddress(newAddress);
+  };
 
-  const displayCelsius = () => {
-      const celsius = (((weatherData?.main.temp - 32) * 5) / 9).toFixed(2);
-      setTemperature(celsius);
-      setUnit('celsius');
-  }
+  const handleSelect = async (selectedAddress: string) => {
+    try {
+      const results = await geocodeByAddress(selectedAddress);
+      const latLng = await getLatLng(results[0]);
+      const { lat, lng } = latLng;
+      setAddress(selectedAddress);
+      searchWeatherData(lat, lng)
+      console.log(lat, lng)
+    } catch (error) {
+      console.error('Error fetching location data:', error);
+    }
+  };
 
   useEffect(() => {
-    axios
-    .get(`https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&cnt=40&appid=e17d9f28655ac27f972639f336659737`)
-      .then((response) => {
-        console.log(response.data)
-        const temperatureData = response.data.list.map((item) => ({
-          time: item.dt,
-          temperature: (((item.main.temp - 273.15) * 9/5) + 32).toFixed(2),
-        }));
-        const windData = response.data.list.map((item) => ({
-          time: item.dt,
-          wind: item.wind.speed
-        }))
-        const minTemperature = Math.min(temperatureData.map((item) => item.temperature))
-        const maxTemperature = Math.max(temperatureData.map((item) => item.temperature))
-        const minDT = Math.min(temperatureData.map((item) => item.dt))
-        const maxDT = Math.max(temperatureData.map((item) => item.dt))
-        setTemperatureChartData(temperatureData);
-        setWindChartData(windData)
-        setMinChartTemperature(minTemperature)
-        setMaxChartTemperature(maxTemperature)
-        setMinChartDT(minDT)
-        setMaxChartDT(maxDT)
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    getWeatherData()
   }, [])
 
   return (
     <>
-    <a href="">Default</a>
-    <a href="">Wind Speed</a>
-    <a href="">Temperature Trends</a>
-    <br />
-    <h2>Wind Speed Trends</h2>
-    <LineChart width={550} height={400} data={windChartData}>
-        <CartesianGrid />
-        <XAxis dataKey={'time'} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="wind" stroke="#8884d8" activeDot={{ r: 5 }} />
-      </LineChart>
-    <br />
-    <h2>Temperature Trends (5 day / 3 hour)</h2>
-    <LineChart width={550} height={400} data={temperatureChartData}>
-        <CartesianGrid />
-        <XAxis dataKey={'time'} domain={[minChartDT, maxChartDT]} />
-        <YAxis domain={[minChartTemperature - 1, maxChartTemperature + 1]} />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 5 }} />
-      </LineChart>
-    <br />
-      <nav>
-        <a href="">Text</a>
-        <a href="">Maps</a>
-      </nav>
-      <p>Location: {weatherData?.name !== '' ? weatherData?.name : 'Remote Area'}</p>
-      <p>Temperature: {temperature}{unit !== null && (unit === 'kelvin' ? ' K' : unit === 'celsius' ? '\u00b0 C' : '\u00b0 F')}</p>
-      <button onClick={displayKelvin}>Kelvin</button>
-      <button onClick={displayFahrenheit}>Fahrenheit</button>
-      <button onClick={displayCelsius}>Celsius</button>
-      <p>Humidity: {weatherData?.main.humidity}</p>
-      <p>Weather: {weatherData?.weather[0].description}</p>
-      <p>Latitude: {weatherData?.coord.lat}</p>
-      <p>Longitude: {weatherData?.coord.lon}</p>
-      <button onClick={getData}>Get Weather Data</button>
-      <button onClick={getCurrentLocationWeatherData}>Get Current Location Weather Data</button>
-      <button onClick={getRandomLocationWeatherData}>Get Random Location Weather Data</button>
-      <br />
-      <input type="text" value={locationQuery} onChange={handleInputChange} />
-      <button>Search By Location</button>
+      <BrowserRouter>
+        <NavigationBar address={address} handleChange={handleChange} handleSelect={handleSelect} />
+        <Routes>
+          <Route path='/' element={<DefaultWeatherView weatherData={weatherData} convertToFahrenheit={convertToFahrenheit} dailyWeatherData={dailyWeatherData} />} />
+          <Route path='compareweather' element={<CompareWeather  convertToFahrenheit={convertToFahrenheit} />} />
+          <Route path='/temperaturetrends' element={<TemperatureTrends weatherData={weatherData} convertToFahrenheit={convertToFahrenheit} />} />
+          <Route path='windspeedtrends' element={<WindSpeedTrends weatherData={weatherData} />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
